bf577ba66fa76834d84a339a3f18c7f3e4af13c5
sky <x_hsky@163.com>
2020-04-21 18:34:57 +0800

    添加nginx安装. 实时远程运行输出

---
+++ bin/nginx.py
@@ -0,0 +1,123 @@
+#!/opt/python3/bin/python3
+# *-* coding:utf8 *-*
+# sky
+
+import sys, os, json
+import tarfile
+import psutil
+
+def config(located, cpu_count, tomcat_servers, webapp):
+    nginx_conf=f'''\
+user  nginx;
+worker_processes  {cpu_count};
+
+#error_log  logs/error.log;
+#error_log  logs/error.log  notice;
+error_log  logs/error.log  info;
+
+pid        logs/nginx.pid;
+worker_rlimit_nofile 65535;
+
+events {{
+    use epoll;
+    worker_connections  65535;
+}}
+
+http {{
+    include       mime.types;
+    default_type  application/octet-stream;
+    charset utf-8;
+
+    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+    #                  '$status $body_bytes_sent "$http_referer" '
+    #                  '"$http_user_agent" "$http_x_forwarded_for"';
+
+    #access_log  logs/access.log  main;
+    server_names_hash_bucket_size 128;
+    client_header_buffer_size 32k;
+    large_client_header_buffers 4 64k;
+
+    client_max_body_size 8m;
+    sendfile        on;
+    tcp_nopush     on;
+    tcp_nodelay on;
+    #keepalive_timeout  0;
+    keepalive_timeout  65;
+
+    # gzip
+    gzip  on;
+    gzip_min_length 1k;
+    gzip_buffers 4 16k;
+    gzip_types text/plain application/x-javascript text/css application/xml;
+    gzip_vary on;
+
+    # load 
+    upstream tomcat_servers {{
+        ip_hash;
+        {tomcat_servers}
+    }}
+
+    server {{
+        listen       80;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+
+        location / {{
+          proxy_pass http://tomcat_servers{webapp};
+          proxy_set_header Host $host; 
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }}
+    }}
+}}
+'''
+
+    nginx_conf_file=f"{located}/nginx/conf/nginx.conf"
+    with open(nginx_conf_file, "w") as nginx_f:
+        nginx_f.write(nginx_conf)
+
+    os.system("id -u nginx &> /dev/null || useradd -r nginx")
+    command=f"cd {located}/nginx ;./sbin/nginx -t &> /dev/null"
+    return os.system(command)
+
+def install(soft_file, located):
+    os.makedirs(located, exist_ok=1)
+
+    try:
+        t=tarfile.open(soft_file)
+        t.extractall(path=located)
+        return 1, "ok"
+    except Exception as e:
+        return 0, e
+
+def main():
+    weight, soft_file, conf_json=sys.argv[1:4]
+    conf_dict=json.loads(conf_json)
+    #print(f"{soft_file=}, {data_dict=}")
+
+    # 安装
+    located=conf_dict.get("located")
+    value, msg=install(soft_file, located)
+    if value==1:
+        print("nginx安装完成")
+    else:
+        print(f"Error: 解压安装包失败: {msg}")
+        return 
+
+    # 配置
+    cpu_count=int(psutil.cpu_count() * float(weight))
+    tomcat_servers=""
+    for i in conf_dict.get("nginx_info").get("proxy_hosts"):
+        tomcat_servers=f"{tomcat_servers}server {i}:8080;\n\t"
+    webapp=conf_dict.get("nginx_info").get("proxy_webapp")
+    value=config(located, cpu_count, tomcat_servers, webapp)
+    if value==0:
+        print("nginx配置优化完成")
+    else:
+        print(f"nginx配置优化失败:{value}")
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# *-* coding:utf8 *-*
+# sky
+
+import json
+from libs.client import Client
+from libs.install import soft
+from itertools import zip_longest
+
+def get_weight():
+    pass
+    return 0.8
+
+def json_ana():
+    pass
+
+def main():
+    arch_file="./config/arch.json"
+    init_file="./config/init.json"
+    conf_file="./config/conf.json"
+    with open(arch_file, "r") as arch_f, open(init_file, "r") as init_f, open(conf_file, "r") as conf_f:
+        try:
+            arch_dict=json.load(arch_f)
+            try:
+                init_dict=json.load(init_f)
+                try:
+                    conf_dict=json.load(conf_f)
+                except json.decoder.JSONDecodeError:
+                    print(f"Error: 配置文件({conf_file})json格式不正确")
+                    exit()
+            except json.decoder.JSONDecodeError:
+                print(f"Error: 配置文件({init_file})json格式不正确")
+                exit()
+        except json.decoder.JSONDecodeError:
+            print(f"Error: 配置文件({arch_file})json格式不正确")
+            exit()
+
+    json_ana()
+
+    # 安装
+    print("开始集群部署...\n")
+    for i in arch_dict.keys():
+        weight=get_weight()
+        for j in arch_dict[i].get("software"):
+            print(f"{i}安装并配置{j}...")
+            port=init_dict[i].get("port")
+            soft_obj=soft(i, port)
+            status=soft_obj.install(j, weight, conf_dict.get(j), f"'{json.dumps(arch_dict.get(i))}'")
+
+            for line in status[1]:
+                if line is not None:
+                    print(line.strip("\n"))
+            for line in status[2]:
+                if line is not None:
+                    print(line.strip("\n"))
+
+    # 启动
+
+
+
+    
+    
+if __name__ == "__main__":
+    main()
