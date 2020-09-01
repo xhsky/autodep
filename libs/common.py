400e514d89b63493d478c1082e8e07f8b49ec0aa
sky <x_hsky@163.com>
2020-09-01 16:53:25 +0800

    增加日志配置和功能

--- libs/common.py
+++ libs/common.py
@@ -2,8 +2,11 @@
 # *-* coding:utf8 *-*
 # sky
 
-import tarfile
+import tarfile, psutil
 import os, time
+import textwrap
+import logging
+from logging import handlers
 
 def port_exist(port, seconds):
     N=0
@@ -14,11 +17,10 @@ def port_exist(port, seconds):
             return 0
         for i in psutil.net_connections():
             if port==i[3][1] and i[6] is not None:
-                print("\n")
                 return 1
         print(".")
 
-def install(soft_file, soft_name, link_dst, pkg_dirs, located):
+def install(soft_file, link_src, link_dst, pkg_dirs, located):
     os.makedirs(located, exist_ok=1)
 
     try:
@@ -28,14 +30,14 @@ def install(soft_file, soft_name, link_dst, pkg_dirs, located):
 
         # 建立软连接
         for i in os.listdir(located):
-            if i.startswith("soft_name"):
-                src=f"{located}/i"
+            if i.startswith(link_src):
+                src=f"{located}/{i}"
                 break
-        dst=f"{located}/link_dst"
+        dst=f"{located}/{link_dst}"
         os.symlink(src, dst)
 
         # 安装依赖
-        if pkg_dirs not None:
+        if pkg_dirs is not None:
             pkgs=" ".join(os.listdir(pkg_dirs))
             command=f"cd {located}/{pkg_dirs} && rpm -Uvh {pkgs} &> /dev/null"
             result=os.system(command)
@@ -43,6 +45,8 @@ def install(soft_file, soft_name, link_dst, pkg_dirs, located):
                 return 1, "ok"
             else:
                 return 0, "安装失败"
+        else:
+            return 1, "ok"
     except Exception as e:
         return 0, e
 
@@ -60,11 +64,61 @@ def config(config_dict):
     }
     """
 
-    for config in conf_dict:
-        with open(config[config_file], "w") as f:
-            f.write(config_context)
+    try:
+        for config in config_dict:
+            context=textwrap.dedent(config_dict[config]["config_context"])
+            with open(config_dict[config]["config_file"], "w") as f:
+                f.write(context)
+        return 1, "ok"
+    except Exception as e:
+        return 0, e
 
-def main():
-    
+class Logger(object):
+    level_relations = {         #日志级别关系映射
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }
+
+    def __init__(self, filename, level, mode="all"):
+        log_to_file=0
+        log_to_console=0
+        log_to_remote=0
+        self.logger=logging.getLogger(filename)
+        self.logger.setLevel(self.level_relations[level])
+
+        if mode=="all":
+            log_to_file=1
+            log_to_console=1
+        elif mode=="file":
+            log_to_file=1
+        elif mode=="console":
+            log_to_console=1
+        elif mode=="remote":
+            log_to_remote=1
+
+        if log_to_file:
+            fmt='%(asctime)s - %(levelname)s: %(message)s'
+            format_str=logging.Formatter(fmt)                           # 设置日志格式
+            self.fh=handlers.TimedRotatingFileHandler(filename=filename, when="D", backupCount=7, encoding='utf-8')
+            self.fh.setFormatter(format_str)                                 # 设置文件里写入的格式
+            self.logger.addHandler(self.fh)                                  # 把对象加到logger里
+        if log_to_console:
+            self.sh=logging.StreamHandler()
+            fmt="%(message)s"
+            #fmt="%(levelname)s: %(message)s"
+            format_str=logging.Formatter(fmt)                           # 设置日志格式
+            self.sh.setFormatter(format_str)
+            self.logger.addHandler(self.sh)                                  # 把对象加到logger里
+        if log_to_remote:
+            self.sh=logging.StreamHandler()
+            #fmt="%(message)s"
+            fmt="%(levelname)s: %(message)s"
+            format_str=logging.Formatter(fmt)                           # 设置日志格式
+            self.sh.setFormatter(format_str)
+            self.logger.addHandler(self.sh)                                  # 把对象加到logger里
+
 if __name__ == "__main__":
     main()
