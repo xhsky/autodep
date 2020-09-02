0153436397339aded9d6b4581b3e72c2e3320158
sky <x_hsky@163.com>
2020-09-02 19:30:48 +0800

    适配新框架

--- bin/elasticsearch.py
+++ bin/elasticsearch.py
@@ -3,33 +3,54 @@
 # Date: 2020年 08月 11日 星期二 15:28:06 CST
 # sky
 
-import sys, os, json, time
-import tarfile
+import sys, os, json
 import psutil
+from libs import common
 
-def config(located, cluster_name, memeber_list, jvm_mem):
-    try:
-        config_text=(
-                f"cluster.name: {cluster_name}\n"
-                #"node.name: es_node\n"
-                "node.master: true\n"
-                "node.voting_only: false\n"
-                "node.data: true\n"
-                "node.ingest: true\n"
-                "bootstrap.memory_lock: true\n"
-                "network.host: 0.0.0.0\n"
-                "http.port: 9200\n"
-                f"discovery.seed_hosts: {memeber_list}\n"
-                "transport.tcp.port: 9300\n"
-                f"cluster.initial_master_nodes: {memeber_list}\n"
-                "gateway.recover_after_nodes: 1\n"
-                "action.destructive_requires_name: true\n"
-                )
-        es_config_file=f"{located}/es/config/elasticsearch.yml"
-        with open(es_config_file, "w") as f:
-            f.write(config_text)
+def main():
+    action, weight, soft_file, conf_json=sys.argv[1:5]
+    conf_dict=json.loads(conf_json)
+    located=conf_dict.get("located")
+    soft_name="ElasticSearch"
+    log=common.Logger(None, "info", "remote")
+    src="elasticsearch-"
+    dst="es"
 
-        jvm_config_file=f"{located}/es/config/jvm.options"
+    # 安装
+    if action=="install":
+        os.system("id -u elastic &> /dev/null || useradd elastic")
+        value, msg=common.install(soft_file, src, dst, None, located)
+        if value==1:
+            log.logger.info(f"{soft_name}安装完成")
+        else:
+            log.logger.error(f"{soft_name}安装失败: {msg}")
+            return 
+
+        # 配置
+        ## es配置
+        mem=psutil.virtual_memory()
+        jvm_mem=int(mem[0] * float(weight) /1024/1024)
+        cluster_name=conf_dict["elasticsearch_info"]["cluster_name"]
+        members_list=conf_dict["elasticsearch_info"]["members"]
+
+        es_config_text=f"""\
+            cluster.name: {cluster_name}
+            #"node.name: es_node
+            node.master: true
+            node.voting_only: false
+            node.data: true
+            node.ingest: true
+            bootstrap.memory_lock: true
+            network.host: 0.0.0.0
+            http.port: 9200
+            discovery.seed_hosts: {members_list}
+            transport.tcp.port: 9300
+            cluster.initial_master_nodes: {members_list}
+            gateway.recover_after_nodes: 1
+            action.destructive_requires_name: true
+        """
+
+        jvm_config_file=f"{located}/{dst}/config/jvm.options"
         with open(jvm_config_file, "r+") as f:
             raw_text=f.readlines()
             xms_index=raw_text.index('-Xms1g\n')
@@ -38,87 +59,60 @@ def config(located, cluster_name, memeber_list, jvm_mem):
             raw_text[xmx_index]=f"-Xmx{jvm_mem}m\n"
             f.seek(0)
             f.writelines(raw_text)
-        return 1
-    except Exception as e:
-        return e
 
-def install(soft_file, located):
-    os.makedirs(located, exist_ok=1)
-    try:
-        t=tarfile.open(soft_file)
-        t.extractall(path=located)
-        return 1, "ok"
-    except Exception as e:
-        return 0, e
+        add_java_file=f"{located}/{dst}/bin/elasticsearch-env"          # 将es自身的java环境写入脚本, 防止与其他JAVA_HOME变量冲突
+        with open(add_java_file, "r+") as f:
+            raw_text=f.readlines()
+            java_home=f"export JAVA_HOME={located}/{dst}/jdk\n"
+            raw_text.insert(2, java_home)
+            f.seek(0)
+            f.writelines(raw_text)
 
-def main():
-    action, weight, soft_file, conf_json=sys.argv[1:5]
-    conf_dict=json.loads(conf_json)
-    located=conf_dict.get("located")
+        ## 环境配置
+        limit_conf_context="""\
+            elastic    -   memlock unlimited
+            elastic    -   fsize   unlimited
+            elastic    -   as  unlimited
+            elastic    -   nofile  65536
+            elastic    -   nproc   65536
+            """
+        sysctl_conf_context="vm.max_map_count=262144"
+        sysctl_conf_file="/etc/sysctl.d/es.conf" 
 
-    # 安装
-    if action=="install":
-        value, msg=install(soft_file, located)
-        if value==1:
-            print("ElasticSearch安装完成")
-        else:
-            print(f"Error: 解压安装包失败: {msg}")
-            return 
-
-        # 配置
-        for i in os.listdir(located):
-            if i.startswith("elasticsearch-"):
-                src=f"{located}/{i}"
-        try:
-            dst=f"{located}/es"
-            # 建立软连接
-            os.symlink(src, dst)
-
-            os.system("useradd elastic")
-            limit_conf="/etc/security/limits.conf"
-            if os.path.exists(limit_conf):
-                limit_conf_context=(
-                        "elastic    -   memlock unlimited\n"
-                        "elastic    -   fsize   unlimited\n"
-                        "elastic    -   as  unlimited\n"
-                        "elastic    -   nofile  65536\n"
-                        "elastic    -   nproc   65536\n"
-                        )
-                with open(limit_conf, "a+") as f:
-                    f.write(limit_conf_context)
+        config_dict={
+                "limit_conf": {
+                    "config_file": "/etc/security/limits.d/elastic.conf", 
+                    "config_context": limit_conf_context
+                    }, 
+                "sysctl_conf": {
+                    "config_file": sysctl_conf_file, 
+                    "config_context": sysctl_conf_context
+                    }, 
+                "es_config_text": {
+                    "config_file": f"{located}/{dst}/config/elasticsearch.yml", 
+                    "config_context": es_config_text
+                    }
+                }
+        result, msg=common.config(config_dict)
+        if result==1:
+            result=os.system(f"chown -R elastic:elastic {located}/{src}* &> /dev/null ; sysctl -p {sysctl_conf_file} &> /dev/null")
+            if result==0:
+                log.logger.info(f"{soft_name}配置完成")
             else:
-                print(f"{limit_conf}文件不存在")
-
-            sysctl_conf="/etc/sysctl.d/es.conf"
-            sysctl_conf_context="vm.max_map_count=262144\n"
-            with open(sysctl_conf, "w+") as f:
-                f.write(sysctl_conf_context)
-
-            os.system(f"chown -R elastic:elastic {src} ; sysctl -p {sysctl_conf} &> /dev/null")
-
-        except Exception as e:
-            print(f"Error: ElasticSearch配置环境变量出错: {e}")
+                log.logger.error(f"{soft_name}环境变量未生效: {result}")
         else:
-            print(f"ElasticSearch配置环境变量完成")
-
-
-        mem=psutil.virtual_memory()
-        jvm_mem=int(mem[0] * float(weight) /1024/1024)
-        cluster_name=conf_dict["elasticsearch_info"]["cluster_name"]
-        members=conf_dict["elasticsearch_info"]["members"]
-        value=config(located, cluster_name, members, jvm_mem)
-        if value==1:
-            print("ElasticSearch配置优化完成")
-        else:
-            print(f"ElasticSearch配置优化失败:{value}")
+            log.logger.error(f"{soft_name}写入配置失败: {msg}")
 
     elif action=="start":
-        command=f"su elastic -l -c 'cd {located}/es ; ./bin/elasticsearch -d -p elasticsearch.pid'" 
+        command=f"su elastic -l -c 'cd {located}/{dst}; ./bin/elasticsearch -d -p elasticsearch.pid'" 
         result=os.system(command)
         if result==0:
-            print("ElasticSearch启动完成")
+            if common.port_exist(9200):
+                log.logger.info(f"{soft_name}启动完成")
+            else:
+                log.logger.error(f"{soft_name}启动超时")
         else:
-            print(f"Error: ElasticSearcht启动失败")
+            log.logger.error(f"{soft_name}启动失败")
 
 if __name__ == "__main__":
     main()
