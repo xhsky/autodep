2628c140e67876a9f662c3e904d7a74f53cbbfc6
sky <x_hsky@163.com>
2020-09-03 20:44:45 +0800

    适配新框架；修复bug

--- bin/mysql.py
+++ bin/mysql.py
@@ -3,224 +3,264 @@
 # sky
 
 import sys, os, json
-import tarfile
 import psutil
-import shutil
-
-def my_cnf_conf(located, N, mem):
-    my_cnf=f""" [mysqld]
-# dir
-datadir={located}/mysql/mydata
-#secure_file_priv=/var/lib/mysql-files
-pid_file={located}/mysql/mydata/mysqld.pid
-
-# network
-#socket=/tmp/mysql.sock
-max_connections=1500
-
-# general set
-lower_case_table_names=1
-default_authentication_plugin=mysql_native_password
-default-time-zone='+08:00'
-wait_timeout=30
-
-# Log 
-## Error Log
-log_error={located}/mysql/mylog/mysqld.log
-log_timestamps=system
-## Slow log
-log_output=file
-slow_query_log=1
-long_query_time=2
-
-# bin log
-server_id={N}
-log_bin={located}/mysql/mylog/binlog/binlog
-binlog_format=row
-binlog_row_event_max_size=8192
-binlog_checksum=crc32
-max_binlog_size=512M
-
-binlog_cache_size=128K
-binlog_stmt_cache_size=32K
-max_binlog_cache_size=8G
-max_binlog_stmt_cache_size=2G
-
-binlog_error_action=abort_server
-binlog_expire_logs_seconds=0
-
-sync_binlog=1
-binlog_group_commit_sync_delay=0
-
-default_storage_engine=innodb
-transaction_write_set_extraction=xxhash64
-
-# innodb
-gtid_mode=on
-enforce_gtid_consistency=1
-## buffer pool
-innodb_buffer_pool_size={mem}M
-innodb_change_buffer_max_size=25
-innodb_buffer_pool_instances=8
-
-## redo log
-innodb_log_group_home_dir={located}/mysql/mylog/redolog
-innodb_log_file_size=256M
-innodb_log_files_in_group=4
-
-## log buffer
-innodb_log_buffer_size=16M
-innodb_flush_log_at_trx_commit=1
-
-## tablespace
-### system tablespace
-innodb_file_per_table=1
-### undo tablespace
-innodb_undo_directory={located}/mysql/mylog/undolog
-innodb_rollback_segments=128
-innodb_max_undo_log_size=1G
-
-
-!include /etc/my_client.cnf
-
-log_slave_updates=1
-[client]
-"""
-
-    my_cnf_file=f"/etc/my.cnf"
-    with open(my_cnf_file, "w") as f:
-        f.write(my_cnf)
-
-def install(soft_file, located):
-    """
-        二进制安装, 需要libaio
-        1.mysql-8.0.19-linux-glibc2.12-x86_64.tar.xz解压
-        2.将liabio放入其中新建的pkg目录
-        3.更改目录名为mysql8并重新压缩
-    """
-    os.makedirs(located, exist_ok=1)
-
-    try:
-        result=os.system("groupadd mysql && useradd -r -g mysql -s /bin/false mysql")
-        if result != 0:
-            return 0, "MySQL: 创建MySQL用户失败"
-
-        t=tarfile.open(soft_file)
-        t.extractall(path=located)
-        libaio_file=os.listdir(f"{located}/mysql8/pkg/")[0]
-        result=os.system(f"cd {located}/mysql8/pkg &> /dev/null && rpm -Uvh {libaio_file} &> /dev/null")
-        # 256为重新安装rpm返回值
-        if result==0 or result==256:
-            return 1, "ok"
-        else:
-            return 0, "MySQL: libaio安装失败"
-    except Exception as e:
-        return 0, e
+from libs import common
 
 def main():
     action, weight, soft_file, conf_json=sys.argv[1:5]
     conf_dict=json.loads(conf_json)
+    soft_name="MySQL"
+    mysql_port=3306
+    log=common.Logger(None, "info", "remote")
 
     mysql_info=conf_dict.get("mysql_info")
     db_info=mysql_info.get("db_info")
     located=conf_dict.get("located")
 
+    my_cnf_file=f"/etc/my.cnf"
+    my_client_cnf_file="/etc/my_client.cnf"
+    os_user="mysql"
+    src="mysql-"
+    dst="mysql"
+    my_data="mydata"
+    my_logs="mylogs"
+
     # 安装
     if action == "install":
-        value, msg=install(soft_file, located)
+        result=os.system(f"id -u {os_user} &> /dev/null || useradd -r -s /bin/false {os_user}")
+        if result != 0:
+            log.logger.error(f"创建{soft_name}用户失败")
+            return
+        value, msg=common.install(soft_file, src, dst, "pkg", located)
         if value==1:
-            print("MySQL安装完成")
+            log.logger.info(f"{soft_name}安装完成")
         else:
-            print(f"Error: MySQL安装失败: {msg}")
+            log.logger.error(f"{soft_name}安装失败: {msg}")
             return 
 
         # 配置
-        mem=psutil.virtual_memory()
-        mem=int(mem[0] * float(weight) /1024/1024)
-
-        server_id=db_info.get("server_id")
-
-        my_cnf_conf(located, server_id, mem)
-
-        mk_dirs_commands=f"mkdir -p {located}/mysql/{{mydata,mylog}}; mkdir -p {located}/mysql/mylog/{{binlog,redolog,undolog,relay}}; chown -R mysql:mysql {located}/mysql ; ln -s {located}/mysql8 /usr/local/mysql && \cp -f {located}/mysql8/support-files/mysql.server /etc/init.d/mysqld && systemctl daemon-reload"
+        mk_dirs_commands=f"mkdir -p {located}/{dst}/{{{my_data},{my_logs}}} && mkdir -p {located}/{dst}/{my_logs}/{{binlog,redolog,undolog,relay}} && chown -R {os_user}:{os_user} {located}/{src}* && ln -snf {located}/{src}* /usr/local/mysql && \cp -f {located}/{dst}/support-files/mysql.server /etc/init.d/mysqld && systemctl daemon-reload"
         result=os.system(mk_dirs_commands)
         if result==0:
-            print("MySQL配置完成")
+            log.logger.info(f"{soft_name}相关目录建立完成")
+            mem=psutil.virtual_memory()
+            mem=int(mem[0] * float(weight) /1024/1024)
+            server_id=db_info.get("server_id")
+
+            mysql_sh_context=f"""\
+                export MySQL_HOME={located}/{dst}
+                export PATH=$MySQL_HOME/bin:$PATH
+            """
+            my_cnf_context=f"""\
+                [mysqld]
+                # dir
+                datadir={located}/{dst}/{my_data}
+                #secure_file_priv=/var/lib/mysql-files
+                pid_file={located}/{dst}/{my_data}/mysqld.pid
+
+                # network
+                #socket=/tmp/mysql.sock
+                max_connections=1500
+
+                # general set
+                lower_case_table_names=1
+                default_authentication_plugin=mysql_native_password
+                default-time-zone='+08:00'
+                wait_timeout=30
+
+                # Log 
+                ## Error Log
+                log_error={located}/{dst}/{my_logs}/mysqld.log
+                log_timestamps=system
+                ## Slow log
+                log_output=file
+                slow_query_log=1
+                long_query_time=2
+
+                # bin log
+                server_id={server_id}
+                log_bin={located}/{dst}/{my_logs}/binlog/binlog
+                binlog_format=row
+                binlog_row_event_max_size=8192
+                binlog_checksum=crc32
+                max_binlog_size=512M
+
+                binlog_cache_size=128K
+                binlog_stmt_cache_size=32K
+                max_binlog_cache_size=8G
+                max_binlog_stmt_cache_size=2G
+
+                binlog_error_action=abort_server
+                binlog_expire_logs_seconds=0
+
+                sync_binlog=1
+                binlog_group_commit_sync_delay=0
+
+                default_storage_engine=innodb
+                transaction_write_set_extraction=xxhash64
+
+                # innodb
+                gtid_mode=on
+                enforce_gtid_consistency=1
+                ## buffer pool
+                innodb_buffer_pool_size={mem}M
+                innodb_change_buffer_max_size=25
+                innodb_buffer_pool_instances=8
+
+                ## redo log
+                innodb_log_group_home_dir={located}/{dst}/{my_logs}/redolog
+                innodb_log_file_size=256M
+                innodb_log_files_in_group=4
+
+                ## log buffer
+                innodb_log_buffer_size=16M
+                innodb_flush_log_at_trx_commit=1
+
+                ## tablespace
+                ### system tablespace
+                innodb_file_per_table=1
+                ### undo tablespace
+                innodb_undo_directory={located}/{dst}/{my_logs}/undolog
+                innodb_rollback_segments=128
+                innodb_max_undo_log_size=1G
+
+                !include {my_client_cnf_file}
+
+                log_slave_updates=1
+                [client]
+            """
+            config_dict={
+                    "mysql_sh":{
+                        "config_file": "/etc/profile.d/mysql.sh", 
+                        "config_context": mysql_sh_context, 
+                        "mode": "w"
+                        }, 
+                    "my_cnf":{
+                        "config_file": my_cnf_file, 
+                        "config_context": my_cnf_context,
+                        "mode": "w"
+                        }
+                    }
+            result, msg=common.config(config_dict)
+            if result==1:
+                log.logger.info(f"{soft_name}配置优化完成")
+            else:
+                log.logger.error(f"{soft_name}配置优化失败: {msg}")
         else:
-            print("Error: MySQL配置失败")
+            log.logger.error(f"{soft_name}相关目录建立失败: {result}")
 
     elif action=="start":
-        cluster_info=mysql_info.get("cluster_info")
         cluster_flag=0
+        cluster_info=mysql_info.get("cluster_info")
         if cluster_info is not None:
             cluster_flag=1
             role=cluster_info.get("role")
             if role == "master":
                 sync_sql=f"create user 'repl'@'%' identified with mysql_native_password by 'DreamSoft_123456'; grant replication slave on *.* to 'repl'@'%';"
             elif role == "slave":
-                my_client_cnf=f"[mysqld]\n#replication\n## master\n## slave\n### relay log\nrelay_log={located}/mysql/mylog/relay/relay\nrelay_log_info_repository=table\n"
-                my_cnf_file="/etc/my_client.cnf"
-                with open(my_cnf_file, "w") as f:
-                    f.write(my_client_cnf)
-                sync_host=cluster_info.get("sync_host")
-                sync_dbs_list=cluster_info.get("sync_dbs")
-                with open(my_cnf_file, "a") as f:
-                    for i in sync_dbs_list:
-                        f.write(f"replicate_do_db={i}\n")
-                sync_sql=f"change master to master_host='{sync_host}',  master_user='repl',  master_password='DreamSoft_123456',  master_auto_position=1; start slave;"
+                my_client_cnf=f"""\
+                    [mysqld]
+                    #replication
+                    ## master
+                    ## slave
+                    ### relay log
+                    relay_log={located}/{dst}/{my_logs}/relay/relay
+                    relay_log_info_repository=table
+                """
 
-        print(f"MySQL初始化中...")
-        init_and_start_command=f"{located}/mysql8/bin/mysqld --initialize --user=mysql && systemctl start mysqld"
-        os.system(init_and_start_command)
+                sync_dbs_list=cluster_info.get("sync_dbs")
+                sync_dbs_config=""
+                for sync_db in sync_dbs_list:
+                    sync_dbs_config=f"{sync_dbs_config}\nreplicate_do_db={sync_db}"
+                config_dict={
+                        "my_client_cnf":{
+                            "config_file": my_client_cnf_file, 
+                            "config_context": my_client_cnf,
+                            "mode": "w"
+                            }, 
+                        "my_sync_db_cnf":{
+                            "config_file": my_client_cnf_file, 
+                            "config_context": sync_dbs_config, 
+                            "mode": "a"
+                            }
+                        }
+
+                result, msg=common.config(config_dict)
+                if result==1:
+                    log.logger.info(f"{role}配置完成")
+                else:
+                    log.logger.error(f"{role}配置失败: {msg}")
+
+                sync_host=cluster_info.get("sync_host")
+                sync_sql=f"change master to master_host='{sync_host}', master_user='repl', master_password='DreamSoft_123456', master_auto_position=1; start slave;"
+
+        log.logger.info(f"{soft_name}初始化中...")
+        init_command=f"{located}/{dst}/bin/mysqld --initialize --user={os_user}"
+        os.system(init_command)
         # 获取密码
         try:
-            with open(f"{located}/mysql/mylog/mysqld.log", "r") as f:
+            with open(f"{located}/{dst}/{my_logs}/mysqld.log", "r") as f:
                 for i in f.readlines():
                     if "temporary password" in i:
+                        log.logger.info(f"{soft_name}初始化成功")
                         pass_line=i.split(" ")
                         init_password=pass_line[-1].strip()
                         break
         except Exception as e:
-            print(f"Error: MySQL初始化失败: {e}")
+            log.logger.error(f"{soft_name}初始化失败: {e}")
             return 1
 
+        start_command=f"systemctl start mysqld"
+        os.system(start_command)
+        if common.port_exist(mysql_port, 20):
+            log.logger.info(f"{soft_name}启动成功")
+        else:
+            log.logger.error(f"{soft_name}启动超时")
+
+        # 更改初始密码
         root_password=db_info.get("root_password")
-        change_pass_command=f"{located}/mysql8/bin/mysqladmin  -uroot -p'{init_password}' password {root_password} &> /dev/null"
+        change_pass_command=f"{located}/{dst}/bin/mysqladmin  -uroot -p'{init_password}' password {root_password} &> /dev/null"
         value=os.system(f"{change_pass_command}")
         if value==0:
-            print("MySQL更改初始密码完成")
+            log.logger.info(f"{soft_name}更改初始密码完成")
         else:
-            print(f"MySQL更改初始密码失败:{value}")
+            log.logger.error(f"{soft_name}更改初始密码失败:{value}")
             return 1
         
         # 创建自定义用户和数据库
         init_sql_list=[]
         for db_name, user_name, user_password in zip(db_info.get("business_db"), db_info.get("business_user"), db_info.get("business_password")):
-            db_sql=f"create database {db_name};"
+            db_sql=f"create database if not exists {db_name};"
             use_sql=f"use {db_name};"
-            user_sql=f"create user '{user_name}'@'%' identified by '{user_password}';"
+            user_sql=f"create user if not exists '{user_name}'@'%' identified by '{user_password}';"
             grant_sql=f"grant all on {db_name}.* to '{user_name}'@'%';"
             init_sql_list.append(db_sql)
             init_sql_list.append(use_sql)
             init_sql_list.append(user_sql)
             init_sql_list.append(grant_sql)
         init_sql=" ".join(init_sql_list)
-        init_commands=f'export MYSQL_PWD="{root_password}" ; echo "{init_sql}" | {located}/mysql8/bin/mysql -uroot'
+        init_commands=f'export MYSQL_PWD="{root_password}" ; echo "{init_sql}" | {located}/{dst}/bin/mysql -uroot'
         value=os.system(init_commands)
         if value==0:
-            print("MySQL用户配置完成")
+            log.logger.info(f"{soft_name}用户配置完成")
         else:
-            print(f"MySQL用户配置失败:{value}")
+            log.logger.error(f"{soft_name}用户配置失败:{value}")
 
         # 集群
         if cluster_flag==1:
-            cluster_commands=f'export MYSQL_PWD="{root_password}" ; echo "{sync_sql}" | {located}/mysql8/bin/mysql -uroot'
+            cluster_commands=f'export MYSQL_PWD="{root_password}" ; echo "{sync_sql}" | {located}/{dst}/bin/mysql -uroot'
             value=os.system(cluster_commands)
             if value==0:
-                print(f"MySQL({role})配置完成")
+                log.logger.info(f"{role}同步完成")
             else:
-                print(f"MySQL({role})配置失败:{value}")
+                log.logger.error(f"{role}同步失败:{value}")
 
 
 if __name__ == "__main__":
+    """
+        二进制安装, 需要libaio
+        1.mysql-8.0.19-linux-glibc2.12-x86_64.tar.xz解压
+        2.将liabio放入其中新建的pkg目录
+        3.更改目录名为mysql8并重新压缩
+    """
     main()
--- bin/rabbitmq.py
+++ bin/rabbitmq.py
@@ -4,75 +4,8 @@
 # sky
 
 import sys, os, json
-import tarfile
-import psutil, time
-
-def config(located, cluster_name, member_list, erlang_mem, node_type):
-    try:
-        config_text=(
-                "listeners.tcp.default = 5672\n"
-                f"vm_memory_high_watermark.absolute={erlang_mem}M\n"
-                "vm_memory_high_watermark_paging_ratio = 0.5\n"
-                "log.file.level = info\n"
-                f"cluster_name={cluster_name}\n"
-                "cluster_formation.peer_discovery_backend = classic_config\n"
-                f"cluster_formation.node_type = {node_type}"
-                #"cluster_formation.classic_config.nodes.1 ={}\n"
-                )
-        for index, item in enumerate(member_list):
-            node_str=f"cluster_formation.classic_config.nodes.{index+1}=rabbit@{item}"
-            config_text=f"{config_text}\n{node_str}"
-        mq_config_file=f"{located}/rabbitmq/etc/rabbitmq/rabbitmq.conf"
-        with open(mq_config_file, "w") as f:
-            f.write(f"{config_text}\n")
-        return 1
-    except Exception as e:
-        return e
-
-def install(soft_file, located):
-    os.makedirs(located, exist_ok=1)
-    try:
-        t=tarfile.open(soft_file)
-        t.extractall(path=located)
-
-        for i in os.listdir(located):
-            if i.startswith("rabbitmq_server-"):
-                src=f"{located}/{i}"
-                break
-
-        # 建立软连接
-        dst=f"{located}/rabbitmq"
-        os.symlink(src, dst)
-
-        # 生成cookie
-        cookie_file="/root/.erlang.cookie"
-        cookie_context="PWSZLCZPGCLCQZXICHRW"
-        with open(cookie_file, "w") as f:
-            os.chmod(cookie_file, 0o400)
-            f.write(cookie_context)
-
-        pkg_dir=f"{located}/rabbitmq/pkg"
-        erlang_file=os.listdir(pkg_dir)[0]
-        result=os.system(f"cd {pkg_dir} ; rpm -Uvh {erlang_file} &> /dev/null")
-        if result==0 or result==256:        # 256为重新安装返回值
-            return 1, "ok"
-        else:
-            return 0, "RabbitMQ: erlang安装失败"
-    except Exception as e:
-        return 0, e
-
-def port_exist(port, seconds):
-    N=0
-    while True:
-        time.sleep(1)
-        N=N+1
-        if N >= seconds:
-            return 0
-        for i in psutil.net_connections():
-            if port==i[3][1] and i[6] is not None:
-                print("\n")
-                return 1
-        print(".")
+import psutil 
+from libs import common
 
 def main():
     """
@@ -81,38 +14,86 @@ def main():
     action, weight, soft_file, conf_json=sys.argv[1:5]
     conf_dict=json.loads(conf_json)
     located=conf_dict.get("located")
+    soft_name="RabbitMQ"
+    rabbitmq_port=5672
+    dst="rabbitmq"
+    log=common.Logger("None", "info", "remote")
 
     # 安装
     if action=="install":
-        value, msg=install(soft_file, located)
+        value, msg=common.install(soft_file, "rabbitmq_server-", dst, "pkg", located)
         if value==1:
-            print("RabbitMQ安装完成")
+            log.logger.info(f"{soft_name}安装完成")
         else:
-            print(f"Error: 解压安装包失败: {msg}")
-            return 
+            log.logger.error(f"{soft_name}安装失败")
 
+        # 配置
+        ## cookie
+        cookie_file="/root/.erlang.cookie"
+        cookie_context="PWSZLCZPGCLCQZXICHRW"
+        # mq配置
         mem=psutil.virtual_memory()
         erlang_mem=int(mem[0] * float(weight) /1024/1024)
         cluster_name=conf_dict["rabbitmq_info"]["cluster_name"]
-        members=conf_dict["rabbitmq_info"]["members"]
+        members_list=conf_dict["rabbitmq_info"]["members"]
         node_type=conf_dict["rabbitmq_info"]["node_type"]
-        value=config(located, cluster_name, members, erlang_mem, node_type)
-        if value==1:
-            print("RabbitMQ配置优化完成")
+
+        mq_config_text=f"""\
+            listeners.tcp.default = {rabbitmq_port}
+            vm_memory_high_watermark.absolute={erlang_mem}M
+            vm_memory_high_watermark_paging_ratio = 0.5
+            log.file.level = info
+            cluster_name={cluster_name}
+            cluster_formation.peer_discovery_backend = classic_config
+            cluster_formation.node_type = {node_type}
+            """
+        mq_config_file=f"{located}/rabbitmq/etc/rabbitmq/rabbitmq.conf"
+
+        members_nodes=""
+        for index, item in enumerate(members_list):
+            members_nodes=f"{members_nodes}\ncluster_formation.classic_config.nodes.{index+1}=rabbit@{item}"
+
+        config_dict={
+                "cookie_config":{
+                    "config_file": cookie_file, 
+                    "config_context": cookie_context, 
+                    "mode": "w"
+                    }, 
+                "mq_config":{
+                    "config_file": mq_config_file, 
+                    "config_context": mq_config_text, 
+                    "mode": "w"
+                    }, 
+                "nodes_config":{
+                    "config_file": mq_config_file, 
+                    "config_context": members_nodes, 
+                    "mode": "a"
+                    }
+                }
+        result, msg=common.config(config_dict)
+        if result == 1:
+            log.logger.info(f"{soft_name}配置优化完成")
         else:
-            print(f"RabbitMQ配置优化失败:{value}")
+            log.logger.error(f"{soft_name}配置优化失败: {msg}")
+
+        # erlang cookie设置权限
+        try: 
+            os.chmod(cookie_file, 0o400)
+            log.logger.info("erlang cookie设置完成")
+        except Exception as e:
+            log.logger.info(f"erlang cookie设置失败: {e}")
 
     elif action=="start":
-        command=f"cd {located}/rabbitmq ; ./sbin/rabbitmq-server -detached" 
+        command=f"cd {located}/{dst} ; ./sbin/rabbitmq-server -detached" 
         result=os.system(command)
         if result==0:
-            status=port_exist(5672, 300)
+            status=common.port_exist(rabbitmq_port)
             if status==1:
-                print("RabbitMQ启动完成")
+                log.logger.info(f"{soft_name}启动完成")
             else:
-                print(f"Error: RabbitMQ启动超时")
+                log.logger.error(f"{soft_name}启动超时")
         else:
-            print(f"Error: RabbitMQ启动失败")
+            log.logger.error(f"{soft_name}启动失败")
 
 if __name__ == "__main__":
     main()
